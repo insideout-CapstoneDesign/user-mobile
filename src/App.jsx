fc8bc776e42f89e984e29dc25fe4285fdffd7368
thisminji <thisminji@dgu.ac.kr>
2026-05-18 20:51:19 +0900

    feat: 로그인/회원가입 Zod 스키마 및 폼 검증 적용 (#17)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import AppLayout from './layouts/AppLayout'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ComponentTestPage from './pages/ComponentTestPage/ComponentTestPage'
+import LoginPage from './pages/Login/LoginPage'
+import SignupPage from './pages/Signup/SignupPage'
 import MapPage from './pages/MapPage/MapPage'
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <AppLayout>
         <Routes>
           <Route path="/" element={<MapPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
           <Route path="/component-test" element={<ComponentTestPage />} />
         </Routes>
       </AppLayout>

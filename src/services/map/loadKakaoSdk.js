201889fe963c7f1ba513dc067ea974d4f8099224
cktmddms <155529880+cktmddms@users.noreply.github.com>
2026-06-12 17:35:46 +0900

    fix: 도면 층 선택 버튼 및 캠퍼스 출입구 연결 수정(#57)

--- src/services/map/loadKakaoSdk.js
+++ src/services/map/loadKakaoSdk.js
@@ -1,6 +1,19 @@
 const KAKAO_SDK_URL = 'https://dapi.kakao.com/v2/maps/sdk.js'
 let kakaoSdkPromise
 
+function createKakaoSdkLoadError() {
+  const origin = window.location.origin
+  const { hostname } = window.location
+  const localDomainHint =
+    hostname === '127.0.0.1'
+      ? ' Kakao Developers에는 localhost와 127.0.0.1이 다른 도메인으로 처리되므로 http://localhost:5173 으로 접속하거나, 현재 origin을 Web 플랫폼 도메인에 추가해주세요.'
+      : ''
+
+  return new Error(
+    `Kakao Maps SDK 로딩에 실패했습니다. 현재 origin: ${origin}.${localDomainHint}`,
+  )
+}
+
 export default function loadKakaoSdk(appKey) {
   if (!appKey) {
     return Promise.reject(new Error('Kakao Maps appKey가 없습니다.'))
@@ -26,7 +39,7 @@ export default function loadKakaoSdk(appKey) {
 
     script.onerror = () => {
       kakaoSdkPromise = null
-      reject(new Error('Kakao Maps SDK 로딩에 실패했습니다.'))
+      reject(createKakaoSdkLoadError())
     }
 
     document.head.appendChild(script)
